ea237c7a21d5f1e07097fd9b55e524aeb8a91be6
Justin Tadlock <justintadlock@gmail.com>
2021-06-23 01:43:51 -0500

    Wrap helper functions in `function_exists()` calls. We need to do this to allow multiple projects to load the repo.

--- src/functions-helpers.php
+++ src/functions-helpers.php
@@ -15,105 +15,111 @@ namespace Hybrid\Media\Meta;
 
 use Hybrid\Proxies\App;
 
-/**
- * Prints media meta directly to the screen.
- *
- * @since  1.0.0
- * @access public
- * @param  string  $property
- * @param  array   $args
- * @return void
- */
-function display( $property, $args = [] ) {
-	echo render( $property, $args );
+if ( ! function_exists( __NAMESPACE__ . '\\display' ) ) {
+	/**
+	 * Prints media meta directly to the screen.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $property
+	 * @param  array   $args
+	 * @return void
+	 */
+	function display( $property, $args = [] ) {
+		echo render( $property, $args );
+	}
 }
 
-/**
- * Returns media meta from a media meta object.
- *
- * @since  1.0.0
- * @access public
- * @param  string  $property
- * @param  array   $args
- * @return string
- */
-function render( $property, array $args = [] ) {
+if ( ! function_exists( __NAMESPACE__ . '\\render' ) ) {
+	/**
+	 * Returns media meta from a media meta object.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $property
+	 * @param  array   $args
+	 * @return string
+	 */
+	function render( $property, array $args = [] ) {
 
-	$html = $label = '';
+		$html = $label = '';
 
-	$args = wp_parse_args( $args, [
-		'post_id'     => get_the_ID(),
-		'tag'         => 'span',
-		'label'       => '',
-		'text'        => '%s',
-		'class'       => 'media-meta__item',
-		'label_class' => 'media-meta__label',
-		'data_class'  => 'media-meta__data',
-		'before'      => '',
-		'after'       => ''
-	] );
+		$args = wp_parse_args( $args, [
+			'post_id'     => get_the_ID(),
+			'tag'         => 'span',
+			'label'       => '',
+			'text'        => '%s',
+			'class'       => 'media-meta__item',
+			'label_class' => 'media-meta__label',
+			'data_class'  => 'media-meta__data',
+			'before'      => '',
+			'after'       => ''
+		] );
 
-	// Append formatted property to class name.
-	if ( ! $args['class'] ) {
+		// Append formatted property to class name.
+		if ( ! $args['class'] ) {
 
-		$args['class'] = sprintf(
-			'media-meta__item media-meta__item--',
-			strtolower( str_replace( '_', '-', $property ) )
-		);
-	}
-
-	// Get the media meta repository for this post.
-	$meta_object = repo( $args['post_id'] );
-
-	// Retrieve the meta value that we want from the repository.
-	$meta = $meta_object->get( $property );
-
-	if ( $meta ) {
-
-		if ( $args['label'] ) {
-
-			$label = sprintf(
-				'<span class="%s">%s</span> ',
-				esc_attr( $args['label_class'] ),
-				$args['label']
+			$args['class'] = sprintf(
+				'media-meta__item media-meta__item--',
+				strtolower( str_replace( '_', '-', $property ) )
 			);
 		}
 
-		$data = sprintf(
-			'<span class="%s">%s</span>',
-			esc_attr( $args['data_class'] ),
-			sprintf( $args['text'], $meta )
-		);
+		// Get the media meta repository for this post.
+		$meta_object = repo( $args['post_id'] );
 
-		$html = sprintf(
-			'<%1$s class="%2$s">%3$s</%1$s>',
-			tag_escape( $args['tag'] ),
-			esc_attr( $args['class'] ),
-			$label . $data
-		);
+		// Retrieve the meta value that we want from the repository.
+		$meta = $meta_object->get( $property );
 
-		$html = $args['before'] . $html . $args['after'];
+		if ( $meta ) {
+
+			if ( $args['label'] ) {
+
+				$label = sprintf(
+					'<span class="%s">%s</span> ',
+					esc_attr( $args['label_class'] ),
+					$args['label']
+				);
+			}
+
+			$data = sprintf(
+				'<span class="%s">%s</span>',
+				esc_attr( $args['data_class'] ),
+				sprintf( $args['text'], $meta )
+			);
+
+			$html = sprintf(
+				'<%1$s class="%2$s">%3$s</%1$s>',
+				tag_escape( $args['tag'] ),
+				esc_attr( $args['class'] ),
+				$label . $data
+			);
+
+			$html = $args['before'] . $html . $args['after'];
+		}
+
+		return $html;
 	}
-
-	return $html;
 }
 
-/**
- * Returns an instance of a media meta repository based on the attachment ID.
- *
- * @since  1.0.0
- * @access public
- * @param  int    $post_id
- * @return Meta
- */
-function repo( $post_id ) {
+if ( ! function_exists( __NAMESPACE__ . '\\repo' ) ) {
+	/**
+	 * Returns an instance of a media meta repository based on the attachment ID.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  int    $post_id
+	 * @return Meta
+	 */
+	function repo( $post_id ) {
 
-	$repositories = App::resolve( 'media/meta' );
+		$repositories = App::resolve( 'media/meta' );
 
-	if ( ! $repositories->has( $post_id ) ) {
+		if ( ! $repositories->has( $post_id ) ) {
 
-		$repositories[ $post_id ] = new Meta( $post_id );
+			$repositories[ $post_id ] = new Meta( $post_id );
+		}
+
+		return $repositories[ $post_id ];
 	}
-
-	return $repositories[ $post_id ];
 }
